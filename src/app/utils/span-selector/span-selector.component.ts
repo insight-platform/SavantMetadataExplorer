830c91871b3f8b09d5b37db96e2d9d4691714f73
Ksenya Arysheva <arysheva_ks@bw-sw.com>
2023-10-19 10:48:54 +0200

    Initial - Update span selector

--- src/app/utils/span-selector/span-selector.component.ts
+++ src/app/utils/span-selector/span-selector.component.ts
@@ -18,63 +18,15 @@ export class SpanSelectorComponent implements OnChanges, ControlValueAccessor {
   @Input() spans: ITreeSpan[];
   spanFormGroup: FormGroup | null;
   showSelector = false;
-  returnForm = (spans: ITreeSpan[]) => {
-    return spans.map((span) => {
-      if (span.children.length) {
-        return {
-          [span.spanID.toString()]: new FormControl(false, { nonNullable: true }),
-          [span.spanID + '_group']: new FormGroup(this.returnForm(span.children)),
-        }
-      } else {
-        return {
-          [span.spanID.toString()]: new FormControl(false, { nonNullable: true }),
-        }
-      }
-    }).reduce((res, field) => ({...res, ...field }), {});
-  };
-  selectControls = (formGroup: FormGroup, value) => {
-    Object.keys(formGroup.controls).forEach(key => {
-      if (key.indexOf('_group') === -1) {
-        formGroup.get(key)?.setValue(value, {emitEvent: false});
-      } else {
-        this.selectControls(formGroup.get(key) as FormGroup, value);
-      }
-    });
-  }
-  returnValue = (value): string[] => {
-    return Object.keys(value).reduce<string[]>((res, key) => {
-      if (key.indexOf('_group') === -1) {
-        return [
-          ...res, value[key] ? key : ''
-        ];
-      } else {
-        return [
-          ...res,
-          ...this.returnValue(value[key]),
-        ];
-      }
-    }, []).filter(key => key !== '');
-  }
 
   ngOnChanges(changes: SimpleChanges) {
     if ('spans' in changes && this.spans.length) {
-      this.spanFormGroup = new FormGroup(this.returnForm(this.spans));
+      this.spanFormGroup = new FormGroup(this._returnForm(this.spans));
       this.showSelector = true;
     } else {
       this.showSelector = false;
       this.spanFormGroup = null;
     }
-  }
-
-  check(value, spanID, formGroup) {
-    if (formGroup) {
-      this.selectControls(formGroup, value);
-    }
-    this._onChange(this.returnValue(this.spanFormGroup?.value));
-  }
-
-  hasIcon(span: ITreeSpan) {
-    return span.tags.find(tag => tag.key === 'error');
   }
 
   registerOnChange(fn: (value: string[]) => void): void {
@@ -86,6 +38,64 @@ export class SpanSelectorComponent implements OnChanges, ControlValueAccessor {
   }
   // eslint-disable-next-line
   writeValue(obj: any): void {
+  }
+
+  check(value, spanID, formGroup) {
+    if (formGroup) {
+      this._selectControls(formGroup, value);
+    }
+    this._onChange(this._returnValue(this.spanFormGroup?.value));
+  }
+
+  hasIcon(span: ITreeSpan) {
+    return this._findErrorTag(span);
+  }
+
+  private _findErrorTag(span: ITreeSpan) {
+    if (span.children.length) {
+      return span.children.some(childSpan => this._findErrorTag(childSpan))
+    }
+    return span.tags.find(tag => tag.key === 'error');
+  }
+
+  private _returnForm(spans: ITreeSpan[]) {
+    return spans.map((span) => {
+      if (span.children.length) {
+        return {
+          [span.spanID.toString()]: new FormControl(false, { nonNullable: true }),
+          [span.spanID + '_group']: new FormGroup(this._returnForm(span.children)),
+        }
+      } else {
+        return {
+          [span.spanID.toString()]: new FormControl(false, { nonNullable: true }),
+        }
+      }
+    }).reduce((res, field) => ({...res, ...field }), {});
+  }
+
+  private _selectControls(formGroup: FormGroup, value) {
+    Object.keys(formGroup.controls).forEach(key => {
+      if (key.indexOf('_group') === -1) {
+        formGroup.get(key)?.setValue(value, {emitEvent: false});
+      } else {
+        this._selectControls(formGroup.get(key) as FormGroup, value);
+      }
+    });
+  }
+
+  private _returnValue(value): string[] {
+    return Object.keys(value).reduce<string[]>((res, key) => {
+      if (key.indexOf('_group') === -1) {
+        return [
+          ...res, value[key] ? key : ''
+        ];
+      } else {
+        return [
+          ...res,
+          ...this._returnValue(value[key]),
+        ];
+      }
+    }, []).filter(key => key !== '');
   }
 
   // eslint-disable-next-line
